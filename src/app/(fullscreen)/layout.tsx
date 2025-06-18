abf48a22f03f6ed5147afc0d3845ccb0833fd10e
ccjr1120 <qianaiwuyu@gmail.com>
2025-06-18 17:39:39 +0800

    ✨ feat: layout op

--- src/app/(fullscreen)/layout.tsx
+++ src/app/(fullscreen)/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import '@/assets/styles/globals.css'
 
 export const metadata: Metadata = {
-  title: '3D 模型查看器 - ccjr1120',
-  description: '在线 3D 模型查看器'
+  title: '匆匆孑然',
+  description: '匆匆孑然'
 }
 
 export default function FullscreenLayout({
